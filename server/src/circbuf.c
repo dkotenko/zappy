d9e6c193418205195074ec4ca48e9cfd45013b86
Gormlaith Melisandre <gmelisan@il-j3.msk.21-school.ru>
2021-09-17 17:44:38 +0300

    Working on reading data from client

--- server/src/circbuf.c
+++ server/src/circbuf.c
@@ -6,44 +6,57 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 21:07:51 by gmelisan          #+#    #+#             */
-/*   Updated: 2021/09/16 21:53:26 by gmelisan         ###   ########.fr       */
+/*   Updated: 2021/09/17 17:35:20 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "circbuf.h"
+#include "logger.h"
 
-void circbuf_push1(t_circbuf circbuf, char data)
+void circbuf_push1(t_circbuf *circbuf, char data)
 {
-	circbuf.buf[iwrite] = data;
-	++circbuf.len;
-	++circbuf.iwrite;
-	if (circbuf.iwrite == CIRCBUF_SIZE)
-		circbuf.iwrite = 0;
-	if (circbuf.len > CIRCBUF_SIZE)
-		circbuf.len = CIRCBUF_SIZE;
+	if (circbuf->len == CIRCBUF_SIZE)
+		log_warning("Circular buffer lost data (%c)",
+					circbuf->buf[circbuf->iwrite]);
+	circbuf->buf[circbuf->iwrite] = data;
+	++circbuf->len;
+	++circbuf->iwrite;
+	if (circbuf->iwrite == CIRCBUF_SIZE)
+		circbuf->iwrite = 0;
+	if (circbuf->len > CIRCBUF_SIZE) {
+		circbuf->len = CIRCBUF_SIZE;
+	}
 }
 
-void circbuf_push(t_circbuf circbuf, char *data, size_t size)
+// TODO circbuf_push memcpy
+void circbuf_push(t_circbuf *circbuf, char *data, size_t size)
 {
-	/* size_t free_space = CIRCBUF_SIZE - circbuf.iwrite; */
-
-	/* while (size > free_space) { */
-	/* 	memcpy(&circbuf.buf[iwrite], data, s); */
-	/* } */
+	for (size_t i = 0; i < size; ++i) {
+		circbuf_push1(circbuf, data[i]);
+	}
 }
 
-char circbuf_pop1(t_circbuf circbuf)
+char circbuf_pop1(t_circbuf *circbuf)
 {
 	char c;
 
-	c = circbuf.buf[iread];
-	--circbuf.len;
-	--circbuf.iread;
-	if (readIndex == CIRCBUF_SIZE)
-		circbuf.iread = 0;	
+	c = circbuf->buf[circbuf->iread];
+	--circbuf->len;
+	--circbuf->iread;
+	if (circbuf->iread == CIRCBUF_SIZE)
+		circbuf->iread = 0;
+	return c;
 }
 
-char *circbuf_pop(t_circbuf circbuf)
+// TODO circbuf_pop memcpy
+char *circbuf_pop(t_circbuf *circbuf)
 {
-
+	static char buf[CIRCBUF_SIZE + 1];
+	int i = 0;
+	
+	while (circbuf->len) {
+		buf[i++] = circbuf_pop1(circbuf);
+	}
+	buf[i] = 0;
+	return buf;
 }
