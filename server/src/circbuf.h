293ae2c988656644fa417f55668351cb3f81b313
Gormlaith Melisandre <gmelisan@oa-a4.msk.21-school.ru>
2021-09-21 18:52:29 +0300

    Working on reading commands, not ready

--- server/src/circbuf.h
+++ server/src/circbuf.h
@@ -6,7 +6,7 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 21:07:43 by gmelisan          #+#    #+#             */
-/*   Updated: 2021/09/21 15:00:12 by gmelisan         ###   ########.fr       */
+/*   Updated: 2021/09/21 18:06:10 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,38 +37,13 @@ void circbuf_clear(t_circbuf *circbuf);
 /* Push one item from `data' to circular buffer */
 void circbuf_push(t_circbuf *circbuf, void *data);
 
+/* Push string to circular buffer */
+void circbuf_push_string(t_circbuf *circbuf, char *str);
+
 /* Pop one item from circular buffer. Use free() after work */
 void *circbuf_pop(t_circbuf *circbuf);
 
 /* Pop all items and concatenate items as string. Use free() after work */
-char *circbuf_pop_all(t_circbuf *circbuf);
-
-/*
-Usage example:
-size = 5
- 0  1  2  3  4
-[ ][ ][ ][ ][ ]		iwrite = 0, iread = 0, len = 0
-
- push a
- [a][ ][ ][ ][ ]		iwrite = 1, iread = 0, len = 1
-
- push b
- [a][b][ ][ ][ ]		iwrite = 2, iread = 0, len = 2			free_space = 3, size = 5
-
- push c
- [a][b][c][ ][ ]		iwrite = 3, iread = 0, len = 3
-
- pop -> a
- [a][b][c][ ][ ]		iwrite = 3, iread = 1, len = 2
-
- pop -> b
- [a][b][c][ ][ ]		iwrite = 3, iread = 2, len = 1
-
- push d, push e, push f
- [f][b][c][d][e]		iwrite = 1, iread = 2, len = 5
-
- pop -> c
- [f][b][c][d][e]		iwrite = 1, iread = 3, len = 5
-*/
+char *circbuf_pop_string(t_circbuf *circbuf);
 
 #endif
